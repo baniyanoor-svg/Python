43d42eba3a1635c146748b3be50f35cb2eb484f4
baniyanoor-svg <baniyanoor@gmail.com>
2025-08-20 22:55:24 +0530

    Add files via upload

---
+++ noor.py
@@ -0,0 +1,10 @@
+
+#CREATE A CALCUALATOR
+a=50
+b=3
+print("The value of ",a,"+",3,"is:",a+b)
+print("The value of ",a,"-",3,"is:",a-b)
+print("The value of ",a,"*",3,"is:",a*b)
+print("The value of ",a,"/",3,"is:",a/b)
+
+
